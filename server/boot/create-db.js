9da014677199d5643d0d9510a7ac2697c49b44d6
Pierre Quételart <pierre@quetelart.fr>
2015-02-27 15:28:36 +0100

    Add categories

--- server/boot/create-db.js
+++ server/boot/create-db.js
@@ -1,25 +1,36 @@
 module.exports = function(app) {
     var mysqlDs = app.dataSources.mysql;
 
-    var names = ["", "FI12","FI13"];
+	var categoryValues = ["Promotions", "Clubs"];
+    var tagValues = ["FI10", "FI11", "FI12","FI13"];
 	//first lets recreate all databases
     mysqlDs.autoupdate(function(){
         console.log("Tables updated: let's seed");
 
 		var Tag = app.models.Tag;
-        var tags = [];
-        for (var i = 1; i < 3; i++) {
-			if(names[i] != ""){
-				tags.push({
-					"label": names[i],
-					"id": i,
-				});
-			}
+		var Category = app.models.Category;
+        
+		var tags = [];
+        for (var i = 0; i < tagValues.length; i++) {
+			tags.push({
+				"label": tagValues[i],
+				"id": i +1,
+			});
+        };
+		
+        var categories = [];
+        for (var i = 0; i < categoryValues.length; i++) {
+			categories.push({
+				"label": categoryValues[i],
+				"id": i +1,
+			});
         };
 		
 		var cb = function(){
 			console.log("OK");
 		}
+		
         Tag.create(tags, cb);      
+		Category.create(categories, cb);
     });
 };
